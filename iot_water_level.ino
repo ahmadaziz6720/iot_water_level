fb7999f1036af616a3b48f733e7c9689150a9329
ahmadaziz6720 <a715210@gmail.com>
2022-03-19 03:38:45 +0700

    update

--- iot_water_level.ino
+++ iot_water_level.ino
@@ -18,8 +18,21 @@
 
 #include "BlynkEdgent.h"
 
+const int trigPin = 5;
+const int echoPin = 18;
+
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
+
 void setup()
 {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
   Serial.begin(115200);
   delay(100);
 
@@ -28,4 +41,22 @@ void setup()
 
 void loop() {
   BlynkEdgent.run();
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
 }
