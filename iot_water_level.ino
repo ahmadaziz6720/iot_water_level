6a1c6179902886fd585f99ad4ebcd30707a4ed13
ahmadaziz6720 <a715210@gmail.com>
2022-03-19 13:33:42 +0700

    -

--- iot_water_level.ino
+++ iot_water_level.ino
@@ -1,6 +1,7 @@
 //Conf Blynk
-#define BLYNK_TEMPLATE_ID "xxxxxx"
-#define BLYNK_DEVICE_NAME "xxxxx"
+#define BLYNK_TEMPLATE_ID "TMPLML2Ny5K1"
+#define BLYNK_DEVICE_NAME "ESP32"
+#define k 0.034
 
 //Library
 #define BLYNK_PRINT Serial
@@ -9,33 +10,39 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
-char auth[] = "xxxxx";
 
-char ssid[] = "xxxx";
-char pass[] = "xxxx";
+char auth[] = "WObJuTK4UMhaJQO7qXFsQdUbe8GB_vI0";
+
+char ssid[] = "TEGUH ATAS";
+char pass[] = "cisitulama49";
 
 //Ultrasonic
 const int trigPin = 5;
 const int echoPin = 18;
-//Variable and Constant
-#define k 0.034
+int state = 0;
+//Variable and Constant` 
 long duration;
 float distance;
 
+int relay =15;
+
+BLYNK_CONNECTED() {
+    Blynk.syncVirtual(V0);
+}
 
 BLYNK_WRITE(V0) {
   int pinValue = param.asInt();
-  digitalWrite(15, pinValue);
+  digitalWrite(relay, pinValue);
 }
 
 BLYNK_READ(V1) //Blynk app has something on V5
 {
-  Blynk.virtualWrite(V1, random(200)); //sending to Blynk
+  Blynk.virtualWrite(V1, distance); //sending to Blynk
 }
 
 void setup()
 {
-  pinMode(15, OUTPUT);
+  pinMode(relay, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   
@@ -48,6 +55,7 @@ void setup()
 void loop() {
   Blynk.run();
   Blynk.virtualWrite(V1, distance);
+  
 
   //Pulses
   digitalWrite(trigPin, LOW);
@@ -59,10 +67,23 @@ void loop() {
   //Duration
   duration = pulseIn(echoPin, HIGH);
   //Distance
-  //distance = duration * k/2;
-
-  //random for demo
-  distance = random(200);
+  distance = duration * k/2;
+  distance = map(distance,30,0,0,100);
+  
+  if(distance < 20){
+    state = 1;
+  }
+  if(distance>=80){
+    state = 0;
+  }
+  if(state ==1&&distance<20){
+    digitalWrite(relay, HIGH);
+    Blynk.virtualWrite(V2, HIGH);
+  }
+  if(state == 0&&distance>80){
+    digitalWrite(relay, LOW);
+    Blynk.virtualWrite(V2, LOW);
+  }
   
   //print
   Serial.print("ketinggian");
